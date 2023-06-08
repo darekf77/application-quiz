9532b3a4cf31c6264cc4feb8c8f631c26e672cf1
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-06-08 11:18:49 +0200

    first

---
+++ environment.js
@@ -0,0 +1,15 @@
+
+const path = require('path')
+var { config } = { config: {} };
+
+config = {
+
+    domain: 'application-quiz.example.domain.com',
+    useDomain: false,
+    title: 'Application Quiz',
+    pwa: {
+        // start_url: ''
+    }
+
+}
+module.exports = exports = { config };
