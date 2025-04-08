91c9e658bdf48101e36c9b5c87abc94db562f0bb
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 21:16:49 +0200

    $$$ update $$$

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'application-quiz.example.domain.com',
+    title: 'Application Quiz',
+  },
+};
+export default env;
